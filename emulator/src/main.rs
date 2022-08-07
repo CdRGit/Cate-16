9407b7a1caa2698e3588891836948d14206f361c
CdRGit <github@catelyn.tech>
2022-08-07 16:04:06 +0200

    non-blocking terminal, achieved

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -1,5 +1,20 @@
-pub mod terminal;
+mod terminal;
+
+use terminal::*;
+use std::io::Read;
+use std::io::Write;
 
 fn main() {
     println!("Hello, world!");
+    let mut term: Terminal = Terminal::make();
+
+    let mut buffer = [0;1];
+    writeln!(term, "Hit a key! ").unwrap();
+    let mut count;
+    for _ in 0..3146875 {
+        count = term.read(&mut buffer).unwrap();
+        if count != 0 {
+            println!("You have hit: {:?} [{}]", buffer, count);
+        }
+    }
 }
--- emulator/src/terminal.rs
+++ emulator/src/terminal.rs
@@ -1,9 +1,58 @@
 extern crate termios;
 
-struct Terminal {
+use std::io;
+use std::io::Read;
+use std::io::Write;
+use termios::{Termios, TCSANOW, ECHO, ICANON, VMIN, tcsetattr};
 
+pub struct Terminal {
+    termios: Termios,
+    reader: std::io::Stdin,
+    stdout: std::io::Stdout,
+}
+
+impl Terminal {
+    pub fn make() -> Self {
+        let stdin = 0;
+
+        let termios = Termios::from_fd(stdin).unwrap();
+        let mut new_termios = termios.clone();
+
+        new_termios.c_cc[VMIN] = 0;
+
+        new_termios.c_lflag &= !(ICANON | ECHO);
+        tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
+        let stdout = io::stdout();
+        let reader = io::stdin();
+
+        Self {termios, reader, stdout}
+    }
 }
 
 impl Drop for Terminal {
-    
+    fn drop(&mut self) {
+        tcsetattr(0, TCSANOW, &self.termios).unwrap();
+
+        println!("Dropping!")
+    }
+}
+
+impl Read for Terminal {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.reader.read(buf)
+    }
+}
+
+impl Write for Terminal {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let res = self.stdout.lock().write(buf)?;
+
+        self.flush()?;
+
+        Ok(res)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.stdout.lock().flush()
+    }
 }
