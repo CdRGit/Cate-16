7ecc73f79ad2690f7acad29f9b96b9e1ff8ee712
CdRGit <github@catelyn.tech>
2022-08-07 20:50:11 +0200

    Starting implementation of system bus

--- emulator/src/machine/bus.rs
+++ emulator/src/machine/bus.rs
@@ -1,5 +1,69 @@
 pub struct Bus {
-    low_ram:   [u8; 512 * 1024],
-    flash_rom: [u8; 512 * 1024],
-    high_ram:  [u8;2048 * 1024],
+    low_ram:   Box<[u8; 512 * 1024]>,
+    flash_rom: Box<[u8; 512 * 1024]>,
+    high_ram:  Box<[u8;2048 * 1024]>,
+}
+
+impl Bus {
+    pub fn new() -> Bus {
+        Bus {
+            low_ram:   Box::new([0u8; 512 * 1024]),
+            flash_rom: Box::new([0u8; 512 * 1024]),
+            high_ram:  Box::new([0u8;2048 * 1024]),
+        }
+    }
+
+    pub fn read(&mut self, bank: u8, addr: u16) -> u8 {
+        match bank {
+            0..=0x0F => {
+                match addr {
+                    0..=0x7EFF => {
+                        self.low_ram[bank as usize * 0x8000 + addr as usize]
+                    }
+                    0x7F00..=0x7FFF => {
+                        panic!("MMIO not implemented yet!!!!");
+                    }
+                    0x8000..=0xFFFF => {
+                        self.flash_rom[bank as usize * 0x8000 + (addr as usize & 0x7FFF)]
+                    }
+                }
+            }
+            0x10..=0x1f => {
+                panic!("Large MMIO not implemented yet!!!!");
+            }
+            0x20..=0x3f => {
+                self.high_ram[(bank - 0x20) as usize * 0x8000 + (addr as usize)]
+            }
+            _ => {
+                panic!("Final 12 MiB not implemented yet!!!!");
+            }
+        }
+    }
+
+    pub fn write(&mut self, bank: u8, addr: u16, value: u8) {
+        match bank {
+            0..=0x0F => {
+                match addr {
+                    0..=0x7EFF => {
+                        self.low_ram[bank as usize * 0x8000 + addr as usize] = value;
+                    }
+                    0x7F00..=0x7FFF => {
+                        panic!("MMIO not implemented yet!!!!");
+                    }
+                    0x8000..=0xFFFF => {
+                        panic!("Write to Flash ROM!!!!");
+                    }
+                }
+            }
+            0x10..=0x1f => {
+                panic!("Large MMIO not implemented yet!!!!");
+            }
+            0x20..=0x3f => {
+                self.high_ram[(bank - 0x20) as usize * 0x8000 + (addr as usize)] = value;
+            }
+            _ => {
+                panic!("Final 12 MiB not implemented yet!!!!");
+            }
+        }
+    }
 }
--- emulator/src/machine/cpu.rs
+++ emulator/src/machine/cpu.rs
@@ -1,40 +1,53 @@
-pub type CPU_FLAGS = u8;
+use super::bus::*;
 
-const CPU_C = 1 << 0;
-const CPU_Z = 1 << 1;
-const CPU_I = 1 << 2;
-const CPU_D = 1 << 3;
-const CPU_X = 1 << 4;
-const CPU_M = 1 << 5;
-const CPU_V = 1 << 6;
-const CPU_N = 1 << 7;
+pub struct Status(pub u8);
 
-#[derive(Debug)]
+const CARRY_FLAG: u8        = 1 << 0;
+const ZERO_FLAG: u8         = 1 << 1;
+const IRQ_FLAG: u8          = 1 << 2;
+const DEC_FLAG: u8          = 1 << 3;
+const SMALL_IDX_FLAG: u8    = 1 << 4;
+const SMALL_ACC_FLAG: u8    = 1 << 5;
+const OVEFLOW_FLAG: u8      = 1 << 6;
+const NEG_FLAG: u8          = 1 << 7;
+
+impl Status {
+    pub fn new() -> Status {
+        Status(SMALL_IDX_FLAG | SMALL_ACC_FLAG | IRQ_FLAG)
+    }
+}
+
 pub struct W65C816 {
-    aLo: u8, aHi: u8,
-    xLo: u8, xHi: u8,
-    yLo: u8, yHi: u8,
-    sLo: u8, sHi: u8,
+    a: u16,
+    x: u16,
+    y: u16,
+    s: u16,
 
-    dbr: u8, dp: u16,
+    dbr: u8,
+    pbr: u8,
 
-    pck: u8, pc: u16,
+    d:  u16,
+    pc: u16,
 
     emu: bool,
-    flags: CPU_FLAGS,
+    flags: Status,
+
+    bus: Bus,
 }
 
 impl W65C816 {
-    pub fn new() -> Self {
+    pub fn new(mut bus: Bus) -> Self {
         W65C816 {
-            aLo: 0, aHi: 0,
-            xLo: 0, xHi: 0,
-            yLo: 0, yHi: 0,
-            sLo: 0, sHi: 0,
-            dbr: 0, dp:  0,
-            pck: 0, pc:  0,
+            a: 0,
+            x: 0,
+            y: 0,
+            s: 0,
+            dbr: 0, pbr: 0,
+            d:   0, pc:  0,
             emu: true,
-            flags: CPU_I,
+            flags: Status::new(),
+            bus,
         }
     }
 }
+
